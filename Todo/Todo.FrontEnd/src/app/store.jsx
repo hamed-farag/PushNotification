0dee0844752854443de6b4a345818ee50c338342
Hamed Farag <hamed.farag.2009@gmail.com>
2017-12-22 20:48:30 +0200

    Add Logger Middleware

--- Todo/Todo.FrontEnd/src/app/store.jsx
+++ Todo/Todo.FrontEnd/src/app/store.jsx
@@ -1,10 +1,32 @@
 import reducer from './reducers';
-import { createStore, applyMiddleware } from 'redux';
+import {
+	createStore,
+	applyMiddleware,
+	compose
+} from 'redux';
 import promise from 'redux-promise';
 
+let enhancer;
+
+if (!__PRODUCTION__) {
+	const createLogger = require('redux-logger');
+	const logger = createLogger();
+
+	enhancer = compose(
+		applyMiddleware(
+			promise,
+			logger
+		),
+		window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+	);
+} else {
+	enhancer = compose(
+		applyMiddleware(
+			promise
+		)
+	);
+}
 export default createStore(
 	reducer,
-	applyMiddleware(
-		promise
-	)
+	enhancer
 );
