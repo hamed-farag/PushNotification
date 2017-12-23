396a933f8648de39d901c4c70d8d3711fca9eb58
Hamed Farag <hamed.farag.2009@gmail.com>
2017-12-23 14:04:45 +0200

    Add Simple Signalr Method

--- Todo/Todo/App_Start/Startup.Auth.cs
+++ Todo/Todo/App_Start/Startup.Auth.cs
@@ -46,6 +46,9 @@ namespace Todo
             // Enable the application to use bearer tokens to authenticate users
             app.UseOAuthBearerTokens(OAuthOptions);
 
+            // Enable SignalR
+            app.MapSignalR();
+
             // Uncomment the following lines to enable logging in with third party login providers
             //app.UseMicrosoftAccountAuthentication(
             //    clientId: "",
